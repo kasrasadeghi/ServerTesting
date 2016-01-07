fb5e2df89fcbc5077ce38356269ba88b2acb6d05
kasrasadeghi <kasra.sadeghi1@gmail.com>
2016-01-07 16:42:40 -0600

    1.0

--- ServerTesting/src/servertesting/MultiServerThread.java
+++ ServerTesting/src/servertesting/MultiServerThread.java
@@ -2,44 +2,41 @@ package servertesting;
 
 import java.io.*;
 import java.net.Socket;
- 
-public class MultiServerThread extends Thread 
-{
+
+public class MultiServerThread extends Thread {
+
     private Socket socket = null;
- 
-    public MultiServerThread(Socket socket) 
-    {
-    super("MultiServer Thread");
-    this.socket = socket;
+
+    public MultiServerThread(Socket socket) {
+        super("MultiServer Thread");
+        this.socket = socket;
     }
- 
-    public void run() 
-    {
- 
-    try 
-    {
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(
+
+    @Override
+    public void run() {
+
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
                     new InputStreamReader(
-                    socket.getInputStream()));
- 
-        String inputLine, outputLine;
-        out.println("Welcome to RPGTown! Please enter a number...");
-        while ((inputLine = in.readLine()) != null) 
-        {
-        	//do something with input and return something to client!
-        	int num = Integer.parseInt(inputLine);
-        	out.println(num + "^2 = " + num*num + "|Pick another number!");
-        if (inputLine.equals("Bye"))
-            break;
+                            socket.getInputStream()));
+
+            String inputLine;
+            out.println("Welcome to RPGTown! Please enter a number...");
+            while ((inputLine = in.readLine()) != null) {
+                //do something with input and return something to client!
+                int num = Integer.parseInt(inputLine);
+                out.println(num + "^2 = " + num * num + "|Pick another number!");
+                if (inputLine.equals("Bye")) {
+                    break;
+                }
+            }
+            out.close();
+            in.close();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        out.close();
-        in.close();
-        socket.close();
- 
-    } catch (IOException e) 
-    {
-        e.printStackTrace();
-    }
     }
 }
